dd5ad24d0fef6ce4ed92ee64eafdca1c6fafc4fb
Marnix Wieffering <17020379@student.hhs.nl>
2018-09-06 15:03:11 +0200

    added reference and 2 tests

--- UnitTestTree/UnitTest.cs
+++ UnitTestTree/UnitTest.cs
@@ -1,15 +1,47 @@
-﻿using System;
+﻿using DSALG_Tree;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestTree
+namespace UnitTestProject1
 {
     [TestClass]
-    public class UnitTest
+    public class UnitTest1
     {
-        // hoi 2
         [TestMethod]
-        public void TestMethod1()
+        public void BinaryTree_DepthTest_GiveTestThree_Get3Depth()
         {
+            // Arange
+            BinaryTree tree = BinaryTree.GetTestTree();
+
+            // Act
+            int diepte = tree.GetDepth();
+
+            // Assert
+            Assert.AreEqual(3, diepte);
+
         }
+
+        [TestMethod]
+        public void BinaryTree_DepthTest_GiveTestThree_Get2Depth()
+        {
+            // Arange
+            BinaryTree tree = new BinaryTree();
+            tree.Insert(5);
+            tree.Insert(3);
+            tree.Insert(7);
+            tree.Insert(4);
+            tree.Insert(2);
+            tree.Insert(6);
+            tree.Insert(8);
+
+            // Act
+            int diepte = tree.GetDepth();
+
+            // Assert
+            Assert.AreEqual(2, diepte);
+        }
+
+
+
     }
 }
