1e2ac14e2b84e2b7191d49d98161091b4e7b30b9
jpaardekooper <p_jazzzp@live.nl>
2018-09-14 13:47:56 +0200

    new windows form app

---
+++ VisualizeGraphDijkstra/VisualizeGraphDijkstra/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Node;
+
+namespace VisualizeGraphDijkstra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Console.WriteLine("New Graph project");
+
+            DirectedGraph graph = DirectedGraph.GetTestGraph(2);
+
+            graph.PrintAllNodes();
+            graph.PrintAllNodeEdges();
+
+            Console.WriteLine(graph.DoesPathExist('E', 'F'));
+        }
+    }
+}
