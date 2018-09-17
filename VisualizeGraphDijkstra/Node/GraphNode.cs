29385d20721466d129d5ae6289f1745e8280bdda
jpaardekooper <p_jazzzp@live.nl>
2018-09-17 10:10:13 +0200

    hardcoded dijkstra

--- VisualizeGraphDijkstra/Node/GraphNode.cs
+++ VisualizeGraphDijkstra/Node/GraphNode.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Node
 {
 
+
     public class GraphNode
     {
         public char Identifier;
--- VisualizeGraphDijkstra/VisualizeGraphDijkstra/Form1.cs
+++ VisualizeGraphDijkstra/VisualizeGraphDijkstra/Form1.cs
@@ -16,7 +16,7 @@ namespace VisualizeGraphDijkstra
     public partial class Form1 : Form
     {
         Point lastPoint = Point.Empty;//Point.Empty represents null for a Point object
-
+        Random rng = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -30,19 +30,25 @@ namespace VisualizeGraphDijkstra
 
             Console.WriteLine(graph.DoesPathExist('E', 'F'));
 
-            DrawPictureBoxToForm(10,10);
-            
+         
+
+            for (int i = 0; i < 9; i++)
+            {
+                DrawPictureBoxToForm(rng.Next(100,400), rng.Next(100,400), i);
+            }
+
         }
       
-        public void DrawPictureBoxToForm(int pointX, int pointY)
-        {      
-            PictureBox Node = new PictureBox();
-            Node.Width = 10;
-            Node.Height = 10;
-            Node.Location = new Point(pointX, pointY);
-            Node.BackColor = Color.Red;
+        public void DrawPictureBoxToForm(int pointX, int pointY, int i)
+        {           
+                PictureBox Node = new PictureBox();
+                Node.Tag = "Node_" + i;
+                Node.Width = 10;
+                Node.Height = 10;
+                Node.Location = new Point(pointX, pointY);
+                Node.BackColor = Color.Red;
 
-            this.Controls.Add(Node);
+                this.Controls.Add(Node);
         }
 
      
@@ -57,12 +63,31 @@ namespace VisualizeGraphDijkstra
             Brush br2 = new SolidBrush(Color.Black);
 
             //Create pen objects
+            Pen p1 = new Pen(br2);
             Pen p2 = new Pen(br2);
+            Pen p3 = new Pen(br2);
+            Pen p4 = new Pen(br2);
+            Pen p5 = new Pen(br2);
+            Pen p6 = new Pen(br2);
+            Pen p7 = new Pen(br2);
+            Pen p8 = new Pen(br2);
 
             //Draw lines
-            gs.DrawLine(p2, new Point(10, 10), new Point(50, 70));
-            gs.DrawLine(p2, new Point(30, 30), new Point(50, 70));
+            gs.DrawLine(p1, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p1, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p2, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p2, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p3, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p3, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p4, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p4, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p5, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p5, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p6, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
+            gs.DrawLine(p6, new Point(rng.Next(100, 400), rng.Next(100, 400)), new Point(rng.Next(100, 400), rng.Next(100, 400)));
         }
+
+   
 
     }
 }
